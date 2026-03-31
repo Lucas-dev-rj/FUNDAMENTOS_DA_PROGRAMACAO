eb23ab4eb68333157c98755e2f016afa3fa562eb
Lucas <l.a.demorais@hotmail.com>
2026-03-31 09:45:22 -0300

    aula_11_Sistema_bancario

---
+++ Estrutura_Sistema_Bancario.py
@@ -0,0 +1,19 @@
+#Solicitar dados do cliente
+idade_cliente= input("digite a sua idade: ")
+salario_cliente= int (input("digite o seu salario (R$): "))
+tempo_trabalho_cliente= int (input("digite o tempo de carteira assinada: "))
+
+
+#verificar aprovação 
+if idade_cliente <= 18
+print(f"emprestimo negado")
+
+elif salario_cliente >= 5000:
+    print(f"Aprovacao automatica")
+
+elif idade_cliente >= 18 and salario_cliente >= 2000 and tempo_trabalho_cliente >= 2:
+    print(f"emprestimo aprovado")
+
+
+else (f"emprestimo negado")
+
